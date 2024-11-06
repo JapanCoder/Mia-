0a0b3383fe4bcc1fe804e50205ca7deac522fc1c
JapanCoder <vpnj648@gmail.com>
2024-11-06 16:36:20 +0000

    add

---
+++ shared_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class EmotionType(Enum):
+    NEUTRAL = "neutral"
+    HAPPY = "happy"
+    SAD = "sad"
+    ANGRY = "angry"
+    STRESSED = "stressed"
+    RELAXED = "relaxed"
+    FEARFUL = "fearful"      # Add FEARFUL
+    SURPRISED = "surprised"  # Add SURPRISED
